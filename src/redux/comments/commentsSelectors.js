708377f9de06836fa53075c39b06ff56825b5c01
katehrybkova <katiushka152701@gmail.com>
2019-09-05 07:41:50 +0300

    Updates

--- src/redux/comments/commentsSelectors.js
+++
@@ -1,5 +0,0 @@
-export const getComments = state =>
-  state.comments;
-
-// DUMMY
-export const dummy = () => null;
--- src/redux/posts/postsSelectors.js
+++
@@ -1,3 +0,0 @@
-export const getAllPosts = state => state.posts.items;
-
-export const getPostWithComments = state => state.posts.item;
