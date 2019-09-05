b8397f4b0c1ebfe105a0c18e879b1426d0291549
katehrybkova <katiushka152701@gmail.com>
2019-09-05 07:34:30 +0300

    Updates

---
+++ precache-manifest.5497e8618e796fca09c7c19019e760f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "836145cce6915e075f5126c326035828",
+    "url": "//posts/index.html"
+  },
+  {
+    "revision": "fa9bc735d30c751a016c",
+    "url": "//posts/static/css/main.d15ff38c.chunk.css"
+  },
+  {
+    "revision": "b65c3f158a06210aefb1",
+    "url": "//posts/static/js/2.73ebe7c6.chunk.js"
+  },
+  {
+    "revision": "fa9bc735d30c751a016c",
+    "url": "//posts/static/js/main.37e068cf.chunk.js"
+  },
+  {
+    "revision": "2d089e5db67646a665f6",
+    "url": "//posts/static/js/runtime~main.516132c3.js"
+  }
+]);
